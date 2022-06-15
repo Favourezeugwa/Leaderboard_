af88f55e74c7a4f24be96a7a8dd74d6557495dc9
Favourezeugwa <favourezeugwa@gmail.com>
2022-06-15 22:27:08 +0100

    Hit the API

--- src/modules/structure.js
+++ src/modules/structure.js
@@ -1,18 +1,51 @@
-const scores = [
-  { name: 'Favour', score: 100 },
-  { name: 'Laurel', score: 80 },
-  { name: 'Hillary', score: 90 },
-  { name: 'Caris', score: 40 },
-  { name: 'Jason', score: 70 },
-  { name: 'Ryan', score: 98 },
-];
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const gameID = 'jRy2HePk1Q0kgeR3yKfj';
+const addScoreURL = `${baseURL + gameID}/scores/`;
 
-function scoreTable() {
+// To create the game ID
+const createGame = async () => {
+  await fetch(baseURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'Favour game',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => (json));
+};
+
+function scoreTable(scores) {
   let output = '';
   scores.forEach((user) => {
-    output += `<li>${user.name}: ${user.score}</li>`;
+    output += `<li>${user.user}: ${user.score}</li>`;
   });
   document.querySelector('.score-table').innerHTML = output;
 }
 
-export default scoreTable;
+// to fetch from the API
+const fetchScore = async () => {
+  await fetch(addScoreURL)
+    .then((response) => response.json())
+    .then((json) => scoreTable(json.result));
+};
+
+// To add score to the API
+const addScore = async (user, score) => {
+  await fetch(addScoreURL, {
+    method: 'POST',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user, score,
+    }),
+  }).then((response) => response.json()).then((json) => {
+    fetchScore();
+  }).catch((error) => (error));
+};
+
+export { addScore, fetchScore };
